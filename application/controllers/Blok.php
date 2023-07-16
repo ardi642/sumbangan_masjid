c33417df75b6a3fae7cf9fc855165d3ece385a37
Ardiansyah Latif <ardiansyahlatif6420@gmail.com>
2023-07-16 15:45:37 +0700

    memperbaiki error karena tidak meload library form_validation

--- application/controllers/Blok.php
+++ application/controllers/Blok.php
@@ -7,7 +7,7 @@ class Blok extends CI_Controller {
 
   public function __construct() {
       parent::__construct();
-      // $this->load->library('form_validation');
+      $this->load->library('form_validation');
       // $this->load->model('M_mahasiswa');
   }
 
